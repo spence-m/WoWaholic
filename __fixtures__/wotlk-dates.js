94cce694f7462e0069b2ec828b9ae5f0c89a4e96
Matthew Spencer <spencer.ma@protonmail.com>
2022-10-06 20:19:33 +0100

    Replaced Burning Crusade Classic calendar with Wrath of the Lich King calendar now the expansion has been released.
    In addition, I've done the following:
    * Made the website title larger.
    * Combined the Season of Mastery and Classic Era calendar as they display the same information.
    * Added jest and a test for the Wrath of the Lich King dates.

---
+++ __fixtures__/wotlk-dates.js
@@ -0,0 +1,68 @@
+export const wrathDates = [
+  [
+    {
+      key: "arathi",
+      date: new Date(2022, 8, 30), // sept
+    },
+    {
+      key: "strand",
+      date: new Date(2022, 9, 7), // oct
+    },
+    {
+      key: "alterac",
+      date: new Date(2022, 9, 14), // oct
+    },
+    {
+      key: "eye",
+      date: new Date(2022, 9, 21), // oct
+    },
+    {
+      key: "warsong",
+      date: new Date(2022, 9, 28), // oct
+    },
+  ],
+  [
+    {
+      key: "arathi",
+      date: new Date(2022, 10, 4), // nov
+    },
+    {
+      key: "strand",
+      date: new Date(2022, 10, 11), // nov
+    },
+    {
+      key: "alterac",
+      date: new Date(2022, 10, 18), // nov
+    },
+    {
+      key: "eye",
+      date: new Date(2022, 10, 25), // nov
+    },
+    {
+      key: "warsong",
+      date: new Date(2022, 11, 2), // dec
+    },
+  ],
+  [
+    {
+      key: "arathi",
+      date: new Date(2022, 11, 9), // dec
+    },
+    {
+      key: "strand",
+      date: new Date(2022, 11, 16), // dec
+    },
+    {
+      key: "alterac",
+      date: new Date(2022, 11, 23), // dec
+    },
+    {
+      key: "eye",
+      date: new Date(2022, 11, 30), // dec
+    },
+    {
+      key: "warsong",
+      date: "",
+    },
+  ],
+];
--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import Alpine from "alpinejs";
+import { getWOTLKHolidays } from "./helpers/date-helper.js";
 
 window.Alpine = Alpine;
 
@@ -44,71 +45,6 @@ function getClassicHolidays(
     }
     if (alteracDate.getFullYear() === year) {
       row[2].date = alteracDate;
-      added = true;
-    }
-
-    if (added) {
-      dates.push(row);
-    }
-    start.setDate(start.getDate() + 28);
-    i++;
-  }
-
-  return dates;
-}
-
-function getBCCHolidays(year) {
-  const eyeOfTheStormEpoch = new Date(2021, 5, 4);
-  const warsongEpoch = new Date(2021, 5, 11);
-  const arathiEpoch = new Date(2021, 5, 18);
-  const alteracEpoch = new Date(2021, 5, 25);
-  const start = new Date(2021, 5, 4);
-
-  let i = 0;
-  const dates = [];
-  while (start.getFullYear() <= year) {
-    const row = [
-      {
-        key: "warsong",
-        date: "",
-      },
-      {
-        key: "arathi",
-        date: "",
-      },
-      {
-        key: "alterac",
-        date: "",
-      },
-      {
-        key: "eye",
-        date: "",
-      },
-    ];
-    const eyeOfTheStormDate = new Date(eyeOfTheStormEpoch);
-    eyeOfTheStormDate.setDate(eyeOfTheStormEpoch.getDate() + 28 * i);
-    const warsongDate = new Date(warsongEpoch);
-    warsongDate.setDate(warsongDate.getDate() + 28 * i);
-    const arathiDate = new Date(arathiEpoch);
-    arathiDate.setDate(arathiDate.getDate() + 28 * i);
-    const alteracDate = new Date(alteracEpoch);
-    alteracDate.setDate(alteracDate.getDate() + 28 * i);
-
-    let added = false;
-    if (eyeOfTheStormDate.getFullYear() === year) {
-      row[0].date = eyeOfTheStormDate;
-      added = true;
-    }
-    if (warsongDate.getFullYear() === year) {
-      row[1].date = warsongDate;
-      added = true;
-    }
-    if (arathiDate.getFullYear() === year) {
-      row[2].date = arathiDate;
-      added = true;
-    }
-    if (alteracDate.getFullYear() === year) {
-      row[3].date = alteracDate;
       added = true;
     }
 
@@ -186,62 +122,22 @@ document.addEventListener("alpine:init", () => {
     isHoliday,
   }));
 
-  Alpine.data("somHolidays", () => ({
+  Alpine.data("wotlkHolidays", () => ({
     year: new Date().getFullYear(),
     dates() {
-      const warsongEpoch = new Date(2021, 10, 19);
-      const arathiEpoch = new Date(2021, 10, 26);
-      const alteracEpoch = new Date(2021, 11, 10);
-      const start = new Date(2021, 10, 19);
-      return getClassicHolidays(
-        this.year,
-        warsongEpoch,
-        arathiEpoch,
-        alteracEpoch,
-        start
-      );
-    },
-    incrementYear() {
-      const ends = 2022;
-      if (this.year < ends) {
-        ++this.year;
-      }
-    },
-    decrementYear() {
-      const epochYear = 2021;
-      if (this.year > epochYear) {
-        --this.year;
-      }
-    },
-    isDecrementDisabled() {
-      const epochYear = 2021;
-      return this.year === epochYear;
-    },
-    isIncrementDisabled() {
-      const ends = 2022;
-      return this.year === ends;
-    },
-    formatDate,
-    isPast,
-    isHoliday,
-  }));
-
-  Alpine.data("bccHolidays", () => ({
-    year: new Date().getFullYear(),
-    dates() {
-      return getBCCHolidays(this.year);
+      return getWOTLKHolidays(this.year);
     },
     incrementYear() {
       ++this.year;
     },
     decrementYear() {
-      const epochYear = 2021;
+      const epochYear = 2022;
       if (this.year > epochYear) {
         --this.year;
       }
     },
     isDecrementDisabled() {
-      const epochYear = 2021;
+      const epochYear = 2022;
       return this.year === epochYear;
     },
     formatDate,
