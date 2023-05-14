f1191f27fa5187fad7a53f26ca540adabb90e6fa
Matthew Spencer <spencer.ma@protonmail.com>
2023-05-14 19:44:30 +0100

    wip.

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   build: {
     rollupOptions: {
       // https://rollupjs.org/guide/en/#big-list-of-options
+      input: {
+        main: resolve(__dirname, "index.html"),
+        nested: resolve(__dirname, "hc/index.html"),
+      },
     },
     minify: "terser",
   },
